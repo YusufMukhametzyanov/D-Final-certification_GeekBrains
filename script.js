ba9638e3f9be561d200e7ea0073b03414d88c0ae
unknown <Y@myrizk.ru>
2023-12-30 21:35:56 +0500

    Добавил меню для адаптивной версии, картинки в формате svg для тела сайта

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const reactEl = document.querySelector(".react");
+const menuEl = document.querySelector(".menuIcon");
+const menuElActive = document.querySelector(".menuIcon2");
+const navForAdaptiveEl = document.querySelector(".navForAdaptive");
+
+document.addEventListener("click", function (e) {
+  if (e.target.className === "menuIcon") {
+    navForAdaptiveEl.style.display = "flex";
+    menuEl.style.display = "none";
+    menuElActive.style.display = "flex";
+  }
+  if (e.target.className === "menuIcon2_") {
+    navForAdaptiveEl.style.display = "none";
+    menuEl.style.display = "flex";
+    menuElActive.style.display = "none";
+  }
+});
+
+// menuEl.addEventListener("click", function (e) {
+//   menuEl.style.display = "none";
+//   menuElActive.style.display = "flex";
+//   navForAdaptiveEl.style.display = "flex";
+// });
+
+// menuElActive.addEventListener("click", function (e) {
+//   menuElActive.style.display = "none";
+//   menuEl.style.display = "flex";
+//   navForAdaptiveEl.style.display = "none";
+// });
+
+navForAdaptiveEl.addEventListener("mouseover", function (e) {
+  menuEl.style.display = "none";
+  menuElActive.style.display = "flex";
+  navForAdaptiveEl.style.display = "flex";
+});
+
+navForAdaptiveEl.addEventListener("mouseout", function (e) {
+  menuElActive.style.display = "none";
+  menuEl.style.display = "flex";
+  navForAdaptiveEl.style.display = "none";
+});
