7017108ca97ca83d3915a4eab9defdbfa4b146c4
unknown <Y@myrizk.ru>
2024-01-07 14:44:32 +0500

    Добавлен функционал для полей ввода пароля и повтора, выделение рамок в мобильной версии

--- script.js
+++ script.js
@@ -67,3 +67,25 @@ passwordEl[1].addEventListener("input", function (e) {
     reTypePasswordEl[1].style.border = "1px solid green";
   }
 });
+
+reTypePasswordEl[0].addEventListener("input", function (e) {
+  if (passwordEl[0].value != reTypePasswordEl[0].value) {
+    passwordEl[0].style.border = "2px solid red";
+    reTypePasswordEl[0].style.border = "2px solid red";
+  }
+  if (passwordEl[0].value === reTypePasswordEl[0].value) {
+    passwordEl[0].style.border = "2px solid green";
+    reTypePasswordEl[0].style.border = "2px solid green";
+  }
+});
+
+passwordEl[0].addEventListener("input", function (e) {
+  if (passwordEl[0].value != reTypePasswordEl[0].value) {
+    passwordEl[0].style.border = "2px solid red";
+    reTypePasswordEl[0].style.border = "2px solid red";
+  }
+  if (passwordEl[0].value === reTypePasswordEl[0].value) {
+    passwordEl[0].style.border = "2px solid green";
+    reTypePasswordEl[0].style.border = "2px solid green";
+  }
+});
